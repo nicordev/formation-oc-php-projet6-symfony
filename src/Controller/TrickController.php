f3afb92c56219a0f991ccd2393e99b6807377b4b
Nicolas <nicolas.renvoise.dev@gmail.com>
2019-03-19 22:15:09 +0100

    Viewing https://www.youtube.com/watch?v=_cgZheTv-FQ&t=1476s

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -4,26 +4,49 @@ namespace App\Controller;
 
 use App\Entity\Trick;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TrickController extends AbstractController
 {
     /**
-     * @Route("/trick/{name}", name="trickShow")
+     * @Route("/trick/{id}", name="trick_show_id", requirements={"id": "\d+"})
+     * @Route("/trick/{name}", name="trick_show_name")
      */
     public function show(Trick $trick)
     {
-//        $repo = $this->getDoctrine()->getRepository(Trick::class);
-//        $trick = null;
-//
-//        if (is_numeric($slug)) {
-//            $trick = $repo->find($slug);
-//        } else {
-//            $trick = $repo->findByName($slug)[0];
-//        }
-
         return $this->render('trick/trick.html.twig', [
             'trick' => $trick
+        ]);
+    }
+
+    /**
+     * @Route("/create-trick", name="create-trick")
+     */
+    public function create()
+    {
+        $trick = new Trick();
+
+        $form = $this->createFormBuilder($trick)
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Nom du trick",
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'placeholder' => "Description du trick",
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('trickGroup')
+            ->getForm();
+
+        return $this->render('trick/create.html.twig', [
+            'createTrickForm' => $form->createView()
         ]);
     }
 }
