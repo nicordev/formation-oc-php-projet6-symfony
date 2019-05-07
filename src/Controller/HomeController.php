5014ac846d9486a1e90726c79ac7eb14acf06695
Nicolas Renvoisé <nicolas.renvoise.dev@gmail.com>
2019-05-07 15:36:47 +0100

    Show next and previous pages of tricks on the home page via AJAX

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,8 @@ use App\Entity\Trick;
 use App\Service\Paginator;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -34,5 +36,26 @@ class HomeController extends AbstractController
             'tricks' => $tricks,
             'paginator' => $paginator
         ]);
+    }
+
+    /**
+     * Get a page of tricks from an ajax call
+     *
+     * @Route("/get-page/{page}", name="home_ajax_get_page", requirements={"page": "\d+"})
+     *
+     * @param ObjectManager $manager
+     * @param Paginator $paginator
+     * @param int $page
+     * @return false|string
+     */
+    public function getPage(ObjectManager $manager, Paginator $paginator, ?int $page = null)
+    {
+        $trickRepo = $manager->getRepository(Trick::class);
+
+        $paginator->update($page ?? 1, self::TRICKS_PER_PAGE, $trickRepo->count([]));
+
+        $tricks = $trickRepo->findBy([], ["createdAt" => "DESC"], $paginator->itemsPerPage, $paginator->pagingOffset);
+
+        return $this->render("home/trickDeck.html.twig", ["tricks" => $tricks]);
     }
 }
