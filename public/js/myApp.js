d57df3fde9c20dfc55a59a0e75cd81a98e84f383
Nicolas Renvoisé <nicolas.renvoise.dev@gmail.com>
2019-05-14 11:59:34 +0100

    Use of Bootstrap jquery for the navigation bar

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,58 +1,5 @@
 var myApp = {
 
-    switchNavigationMenu: function (event) {
-
-        var navigationMenuElt = document.getElementById("navigation-menu");
-
-        if (navigationMenuElt) {
-            navigationMenuElt.remove();
-        } else {
-            buildNavigationMenu(event.target.parentElement);
-        }
-
-        /**
-         * Create a navigation menu
-         *
-         * @param parentElt
-         */
-        function buildNavigationMenu(parentElt) {
-
-            var navigationLinkElts = document.getElementsByClassName("nav-link"),
-                menuWrapperElt = document.createElement("div"),
-                menuElt = document.createElement("ul");
-
-            menuElt.classList.add("list-group");
-            for (let navLinkElt of navigationLinkElts) {
-                menuElt.appendChild(buildMenuItem(navLinkElt));
-            }
-
-            menuWrapperElt.classList.add("main-navigation-menu");
-            menuWrapperElt.appendChild(menuElt);
-            menuWrapperElt.id = "navigation-menu";
-
-            parentElt.parentElement.appendChild(menuWrapperElt);
-
-            /**
-             * Make a li element with a link
-             *
-             * @param linkElt
-             * @param menuElt
-             */
-            function buildMenuItem(linkElt) {
-
-                let liElt = document.createElement("li"),
-                    linkCloneElt = linkElt.cloneNode(true);
-
-                liElt.classList.add("list-group-item");
-                liElt.classList.add("background-color-1");
-                linkCloneElt.classList.add("color-1");
-                liElt.appendChild(linkCloneElt);
-
-                return liElt;
-            }
-        }
-    },
-
     eventTarget: {
 
         /**
